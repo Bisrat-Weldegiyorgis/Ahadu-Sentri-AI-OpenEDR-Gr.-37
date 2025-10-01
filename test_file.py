075bfa7fe17ada7898590d40db948b40d887f02a
Bisrat-Weldegiyorgis <bisratweldegiyorgis@gmail.com>
2025-10-01 08:13:13 +0300

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,3 +1,5 @@
+from app import predict
 
-def test_dummy():
-    assert 1 + 1 == 2
+def test_predict_output_type():
+    result = predict([5.1, 3.5, 1.4, 0.2])
+    assert isinstance(result, int) or isinstance(result, str)
